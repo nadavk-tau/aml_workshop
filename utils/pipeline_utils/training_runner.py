e502c619f721ed76ca489046dee67913c45c711f
Nadav Kra-Oz <nadavkraoz@mail.tau.ac.il>
2021-12-02 22:14:41 +0200

    Fixing run_cross_validation

--- utils/pipeline_utils/training_runner.py
+++ utils/pipeline_utils/training_runner.py
@@ -1,11 +1,9 @@
 import pandas as pd
 
-from joblib import parallel_backend, save
-from typing import Callable
-from sklearn.linear_model import Lasso, MultiTaskLasso
-from sklearn.model_selection import RepeatedKFold, train_test_split, GridSearchCV
+from joblib import parallel_backend
+from sklearn.linear_model import MultiTaskLasso
+from sklearn.model_selection import cross_validate, train_test_split, GridSearchCV
 from sklearn.multioutput import MultiOutputRegressor
-from sklearn.metrics import mean_squared_error
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import SelectFromModel
 from sklearn.pipeline import Pipeline
@@ -14,6 +12,7 @@ from scipy.stats import spearmanr
 
 class TrainingRunner(object):
     RANDOM_STATE = 10
+    VERBOSITY = 3
 
     def __init__(self, pipeline: Pipeline, features_data: pd.DataFrame, target_data: pd.DataFrame):
         self._pipeline = pipeline
@@ -22,25 +21,14 @@ class TrainingRunner(object):
 
     def run_grid_search_cv(self, tuned_parameters, number_of_splits, scoring='neg_mean_squared_error'):
         X_train, _, y_train, _ = train_test_split(self.X, self.y, random_state=self.RANDOM_STATE) 
-        clf = GridSearchCV(self._pipeline, tuned_parameters, cv=number_of_splits, scoring=scoring, verbose=3)
+        clf = GridSearchCV(self._pipeline, tuned_parameters, cv=number_of_splits, scoring=scoring, verbose=self.VERBOSITY)
         with parallel_backend('loky'):
             res = clf.fit(X_train, y_train)
         return res
 
-    def run_cross_validation(self, number_of_splits: int, number_of_repeats: int, accuracy_function: Callable = mean_squared_error) -> pd.DataFrame:
-        rkf = RepeatedKFold(n_splits=number_of_splits, n_repeats=number_of_repeats)
-
-        error_measures = []
-        for train_index, test_index in rkf.split(self.X):
-            X_train, X_test = self.X.iloc[train_index], self.X.iloc[test_index]
-            y_train, y_test = self.y.iloc[train_index], self.y.iloc[test_index]
-
-            clf = self._pipeline.fit(X_train, y_train)
-            y_pred = clf.predict(X_test)
-
-            current_mse = accuracy_function(y_test, y_pred)
-            error_measures.append(current_mse)
-        return pd.DataFrame(error_measures)
+    def run_cross_validation(self, number_of_splits: int, scoring: str) -> pd.DataFrame:
+        with parallel_backend('loky'):
+            return cross_validate(self._pipeline, self.X, self.y, cv=number_of_splits, scoring=scoring, verbose=self.VERBOSITY) 
 
 
 class SpearmanCorrelationPipelineRunner(TrainingRunner):
@@ -56,8 +44,8 @@ class ModelFeatureSlectionPipelineRunner(TrainingRunner):
                              ('training_model', MultiOutputRegressor(training_model))])
         super().__init__(pipeline, features_data, target_data)
 
+
 class LassoPipelineRunner(TrainingRunner):
     def __init__(self, features_data: pd.DataFrame, target_data: pd.DataFrame, alpha: float = 1.0):
-        # pipeline = Pipeline([('training_mode', MultiOutputRegressor(Lasso(alpha=alpha)))])
-        pipeline = Pipeline([('training_mode', MultiTaskLasso(alpha=alpha))])
+        pipeline = Pipeline([('training_mode', MultiTaskLasso(random_state=10, max_iter=10000, alpha=alpha))])
         super().__init__(pipeline, features_data, target_data)
