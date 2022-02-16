6e654d3d3b698f8850d1e173bd2587f58ef2fe70
Nadav Kra-Oz <nadavkraoz@mail.tau.ac.il>
2022-02-16 12:44:41 +0200

    Adding fix to path_consts.py

--- config/path_consts.py
+++ config/path_consts.py
@@ -1,5 +1,8 @@
 import pathlib
-PROJECT_ROOT_PATH = pathlib.Path(__file__, '..')
+
+# First '.parent' for 'config' dir, second for the project's root path
+PROJECT_ROOT_PATH = pathlib.Path(__file__).parent.parent.resolve()
+
 DATA_FOLDER_PATH = pathlib.Path(PROJECT_ROOT_PATH, r'data')
 RESULTS_FOLDER_PATH = pathlib.Path(PROJECT_ROOT_PATH, r'results')
 TASK3_FEATURES_PATH = pathlib.Path(PROJECT_ROOT_PATH, r'data/task3_features')
